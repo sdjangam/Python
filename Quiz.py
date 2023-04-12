103bbe4db00f17edb7dde87ba7c65a839c231930
Shubham Jangam <67232573+sdjangam@users.noreply.github.com>
2023-04-12 13:32:22 +0530

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,71 @@
+print("Welcome to the GK Quiz")
+
+print("|How many continents are there in the world?|")
+
+print("|Please select your option|")
+
+print("| 1. Six         |")
+print("| 2. Seven       |")
+print("| 3. Eight       |")
+
+choice = input("Enter 1, 2, or 3:")
+
+if choice == "1":
+	print("Wrong answer.")
+
+elif choice == "2":
+	print("That's the correct answer.")
+
+elif choice == "3":
+	print("Wrong answer.")
+
+else:
+	print("Incorrect option.")
+
+print("|In which city would you find the world's tallest building?|")
+
+print("|Please select your option|")
+
+print("| 1. New York City        |")
+print("| 2. Japan                |")
+print("| 3. Dubai                |")
+
+choice = input("Enter 1, 2 or 3:")
+
+if choice == "1":
+	print("Wrong answer.")
+
+elif choice == "2":
+	print("Wrong answer.")
+
+elif choice == "3":
+	print("That's the correct answer..")
+
+else:
+	print("Incorrect option.")
+
+
+print("|Which is the largest continent  |")
+
+print("|Please select your option|")
+
+print("| 1. Antartica            |")
+print("| 2. Asia                 |")
+print("| 3. Australia            |")
+
+choice = input("Enter 1, 2 or 3:")
+
+if choice == "1":
+	print("Wrong answer")
+
+elif choice == "2":
+	print("That's the correct answer")
+
+elif choice == "3":
+	print("Wrong answer")
+
+else:
+	print("Incorrect option.")
+
+
+print("Thank you for attending the Quiz Activity...")
